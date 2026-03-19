92724503bacbb84fd004ceeec24fa02d9aa902b4
greencode-dev <antonioverde.dev@gmail.com>
2026-03-19 18:22:10 +0100

    feat: Implement MySQL database connection pool using mysql2/promise.

--- data/db.js
+++ data/db.js
@@ -4,8 +4,8 @@ const dbConfig = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 };
 
 const dbConnection = mysql.createPool(dbConfig);
